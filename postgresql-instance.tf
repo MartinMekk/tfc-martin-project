d1f160299dfc9797c5d789c8bdd736b30b39ee04
Martin Solheim <martin.solheim@me.com>
2022-08-19 14:51:13 +0200

    Configure postgresql instance

---
+++ postgresql-instance.tf
@@ -0,0 +1,59 @@
+resource "google_sql_database_instance" "postgresql" {
+  name = "martin-project-db1"
+  project = var.gcp_project_id
+  region = var.gcp_region
+  database_version = var.db_version
+
+  settings {
+    tier = var.db_tier
+    activation_policy = var.db_activation_policy
+    disk_autoresize = var.db_disk_autoresize
+    disk_size = var.db_disk_size
+    disk_type = var.db_disk_type
+    pricing_plan = var.db_pricing_plan
+
+    location_preference {
+      zone = var.gcp_zone
+    }
+
+    maintenance_window {
+      day = "7" # sunday
+      hour = "3"
+    }
+
+    backup_configuration {
+      binary_log_enabled = true
+      enabled = true
+      start_time = "00:00"
+    }
+
+    ip_configuration {
+      ipv4_enabled = true
+      authorized_networks {
+        value = var.db_instance_access_cidr
+      }
+    }
+  }
+}
+
+# create database
+resource "google_sql_database" "postgresql_db" {
+  name = var.db_name
+  project = var.gcp_project_id
+  instance = google_sql_database_instance.postgresql.name
+  charset = var.db_charset
+  collation = var.db_collation
+}
+
+# create user
+resource "random_id" "user_password" {
+  byte_length = 32
+}
+
+resource "google_sql_user" "postgresql_user" {
+  name = var.db_user_name
+  project = var.gcp_project_id
+  instance = google_sql_database_instance.postgresql.name
+  host = var.db_user_host
+  password = var.db_user_password == "" ? random_id.user_password.hex : var.db_user_password
+}

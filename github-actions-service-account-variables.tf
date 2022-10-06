07cb72ce073560300812c2ca5238fc15b4e4c2bf
Martin Solheim <martin.solheim@me.com>
2022-10-06 14:12:23 +0200

    Oppdaterer identity pool oppsett
    - Bruker variabler i stedet for å sette verdiene i filen.
    - Bruker riktig reponavn i iam binding members.
    - Bytter navn på pool og setter på deletion protection på ressursen
    siden om man sletter en pool vil den fremdeles eksistere med status
    deleted i 30 dager. Det gjør at terraform feiler i å oprette resursen
    hvis man applyer på nytt.

--- github-actions-service-account-variables.tf
+++ github-actions-service-account-variables.tf
@@ -1,19 +1,35 @@
-variable "sa_account_id" {
+variable "workload_identity_pool_sa_account_id" {
   description = "Account ID for GitHub Actions service account"
   default     = "github-actions-service-account"
 }
 
-variable "sa_display_name" {
+variable "workload_identity_pool_sa_display_name" {
   description = "Display name for GithUb Actions service account"
   default     = "Github Actions Service Account"
 }
 
-variable "sa_workload_identity_pool_id" {
+variable "workload_identity_pool_id" {
   description = "The ID for the identity pool used for federating with GitHub Actions"
-  default     = "github-actions-pool"
+  default     = "github-actions-identity-pool"
 }
-variable "sa_pool_display_name" {
+
+variable "workload_identity_pool_display_name" {
   description = "Display name for the identity pool used for federating with GitHub Actions"
   default     = "GitHub Actions Pool"
+}
+
+variable "workload_identity_pool_provider_id" {
+  description = "The ID for the provider used for federating with GitHub Actions"
+  default     = "github-actions-poolprovider"
+}
+
+variable "workload_identity_pool_provider_display_name" {
+  description = "The Display name for the provider used for federating with GitHub Actions"
+  default     = "GitHub Actions Provider"
+}
+
+variable "workload_identity_pool_binding_gh_repo" {
+  description = "The name of the GitHub repo that should be federated with"
+  default = "martin-platform/backstage"
 }
 
--- github-actions-service-account.tf
+++ github-actions-service-account.tf
@@ -1,17 +1,20 @@
 resource "google_service_account" "github-actions-sa" {
-  account_id   = "github-actions-service-account"
-  display_name = "GithubActions Service Account"
+  account_id   = var.workload_identity_pool_sa_account_id
+  display_name = var.workload_identity_pool_sa_display_name
 }
 
 resource "google_iam_workload_identity_pool" "github-actions-pool" {
-  workload_identity_pool_id = "github-actions-pool-2"
-  display_name              = "GitHub Actions Pool"
+  workload_identity_pool_id = var.workload_identity_pool_id
+  display_name              = var.workload_identity_pool_display_name
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "google_iam_workload_identity_pool_provider" "github-actions-provider" {
   workload_identity_pool_id          = google_iam_workload_identity_pool.github-actions-pool.workload_identity_pool_id
-  workload_identity_pool_provider_id = "github-actions-provider"
-  display_name                       = "GitHub Actions Provider"
+  workload_identity_pool_provider_id = var.workload_identity_pool_provider_id
+  display_name                       = var.workload_identity_pool_provider_display_name
   attribute_mapping                  = {
     "google.subject"       = "assertion.sub"
     "attribute.actor"      = "assertion.actor"
@@ -24,7 +27,7 @@ resource "google_iam_workload_identity_pool_provider" "github-actions-provider" 
 
 resource "google_project_iam_binding" "github-actions-binding" {
   members = [
-    "principalSet://iam.googleapis.com/projects/${var.gcp_project_id}/locations/global/workloadIdentityPools/${google_iam_workload_identity_pool.github-actions-pool.id}/attribute.full/${var.gh_repo}"
+    "principalSet://iam.googleapis.com/projects/${var.gcp_project_id}/locations/global/workloadIdentityPools/${google_iam_workload_identity_pool.github-actions-pool.id}/attribute.full/${var.workload_identity_pool_binding_gh_repo}"
   ]
   project = var.gcp_project_id
   role    = "roles/iam.workloadIdentityUser"

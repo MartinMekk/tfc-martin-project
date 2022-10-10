52774cca42210bbd888452e730dac92f8b5b7726
Martin Solheim <martin.solheim@me.com>
2022-10-10 16:27:54 +0200

    Forsøker å bruke name attributten til service account i auth oppsett
    Misstenker at det har blitt brukt feil verdi her siden actions som
    bruker autentiseringen ikke får til å gjøre impersonation.

--- github-actions-service-account.tf
+++ github-actions-service-account.tf
@@ -1,18 +1,9 @@
-locals {
-  github_auth_to_gcp_sa_mapping = {
-    github-actions-container-push = {
-      sa_name   = google_service_account.github-actions-sa.id
-      attribute = "attribute.repository/${var.gh_repo}"
-    }
-  }
-}
-
 resource "google_service_account" "github-actions-sa" {
   account_id   = var.workload_identity_pool_sa_account_id
   display_name = var.workload_identity_pool_sa_display_name
 }
 
-resource "google_project_iam_member" "gtithub-actions-sa-iam" {
+resource "google_project_iam_member" "github-actions-sa-iam" {
   member  = "serviceAccount:${google_service_account.github-actions-sa.email}"
   project = var.gcp_project_id
   role    = "roles/artifactregistry.writer"
@@ -24,5 +15,10 @@ module "github_actions_auth" {
   pool_id     = var.workload_identity_pool_id
   provider_id = var.workload_identity_pool_provider_id
   project_id  = var.gcp_project_id
-  sa_mapping  = local.github_auth_to_gcp_sa_mapping
+  sa_mapping  = {
+    "github-actions-sa" = {
+      sa_name   = google_service_account.github-actions-sa.name
+      attribute = "attribute.repository/${var.gh_repo}"
+    }
+  }
 }
